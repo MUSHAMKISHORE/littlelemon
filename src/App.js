bd4d6328231d9dd06172815409630024f5ca3b47
kishore <kishcbit@gmail.com>
2023-04-20 12:05:25 +0530

    semmantic tags example

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
+    <>
+    <head>
+      <meta name='og:title' content='little lemon' />
+      <meta name='0g:description' content='This is famous restuarent in this area, lets have a good food'/>
+    </head>
+    <header>
+      <nav>
+        <ul>
+          <li><a href='./App.js'>HOME</a></li>
+          <li><a href='./App.js'>ABOUT</a></li>
+          <li><a href='./App.js'>GRIDS</a></li>
+          <li><a href='./App.js'>CONTACTS</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <h2>This is main</h2>
+    </main>
+    <footer>@copyright</footer>
+    </>
   );
 }
 
